726110abae3ec954ea0891bc371a1be203a7b0b3
ilovekego <ilovekenya001@gmail.com>
2026-09-25 18:21:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -57,9 +57,9 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: { 
-        secure: process.env.NODE_ENV === 'production',
+        secure: false, // --- THE FINAL FIX: FORCE COOKIE OVER PROXY ---
         maxAge: 1000 * 60 * 60 * 24,
-        sameSite: 'lax' // Ensures OAuth redirects keep the cookie
+        sameSite: 'lax'
     } 
 }));
 
